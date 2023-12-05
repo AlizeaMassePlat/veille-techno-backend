bbf0d17915824e9af0b783051173689da0243656
AlizeaMassePlat <alizea.masse@gmail.com>
2023-12-05 14:49:04 +0100

    register feature

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserEntity } from './entity/user.entity';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('user')
 export class UserController {
@@ -9,5 +10,10 @@ export class UserController {
   @Get()
   getUser(user: UserEntity): UserEntity {
     return user;
+  }
+
+  @Post('register')
+  register(@Body() dto: RegisterDto): Promise<UserEntity> {
+    return this.userService.register(dto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
-import { Repository } from 'typeorm';
+import { Repository, QueryFailedError } from 'typeorm';
+import { UserEntity } from './entity/user.entity';
+import { RegisterDto } from './dto/register.dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class UserService {
@@ -9,4 +12,24 @@ export class UserService {
     @InjectRepository(User)
     private readonly userRepo: Repository<User>,
   ) {}
+
+  public async register(dto: RegisterDto): Promise<UserEntity> {
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.userRepo.save({
+        firstName: dto.firstName,
+        lastName: dto.lastName,
+        email: dto.email,
+        hash: hash,
+      });
+
+      return new UserEntity(user);
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        throw new ForbiddenException(`Email ${dto.email} is already in use`);
+      }
+
+      throw error;
+    }
+  }
 }
